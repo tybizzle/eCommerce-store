6eb69447bb594d6031b9b5038385fb534bc3e8f4
tybizzle <akinolataye831@gmail.com>
2020-10-02 16:20:06 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/cart";
+import Default from "./components/Default";
+import Modal from "./components/Modal";
+
+function App() {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={ProductList} />
+          <Route exact path='/details' component={Details} />
+          <Route exact path='/cart' component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        <Modal />
+      </Fragment>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ProductProvider } from "./context";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ProductProvider>
+      <App />
+    </ProductProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
